df3bbd67702528ede0ce17a75f98e179e71fab96
Tadija Lazarevic <lazarevic.tadija@gmail.com>
2014-11-20 22:22:09 +0100

    Promenio kontroler

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,5 +1,5 @@
 class ListingsController < ApplicationController
-  before_action :set_listing, only: [:show, :edit, :update, :destroy]
+   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   # GET /listings
   # GET /listings.json
